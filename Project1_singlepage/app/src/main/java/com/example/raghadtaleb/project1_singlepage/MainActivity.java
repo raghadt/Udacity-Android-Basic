a66f74419d9b73a8eb65688b5ecc7a8880466b79
Raghad <raghad.taleb@hotmail.com>
2017-11-19 13:39:59 +0300

    Project 1 - Single Page App
    Business card for a web deisgn company

---
+++ Project1_singlepage/app/src/main/java/com/example/raghadtaleb/project1_singlepage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.raghadtaleb.project1_singlepage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
